99d8db3cfd41baf90b4afb964cd965ce80e924b1
James Newton-King <james@newtonking.com>
2020-04-20 20:46:10 +1200

    Add CCore server

---
+++ GrpcCoreServer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Core;
+using GrpcSampleServer;
+
+namespace GrpcCoreServer
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var server = new Server
+            {
+                Services = { GrpcSample.Greeter.BindService(new GreeterService()) }
+            };
+
+            var host = "0.0.0.0";
+            var port = 5001;
+            server.Ports.Add(host, port, ServerCredentials.Insecure);
+            Console.WriteLine("Running server on " + string.Format("{0}:{1}", host, port));
+            server.Start();
+
+            await server.ShutdownTask;
+        }
+    }
+}
--- GrpcSampleServer/Services/GreeterService.cs
+++ GrpcSampleServer/Services/GreeterService.cs
@@ -1,24 +1,17 @@
+using System.Threading.Tasks;
 using Grpc.Core;
 using GrpcSample;
-using Microsoft.Extensions.Logging;
-using System.Threading.Tasks;
 
 namespace GrpcSampleServer
 {
     public class GreeterService : Greeter.GreeterBase
     {
-        private readonly ILogger<GreeterService> _logger;
-        public GreeterService(ILogger<GreeterService> logger)
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
         {
-            _logger = logger;
-        }
-
-        public override async Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
-        {
-            return new HelloReply
+            return Task.FromResult(new HelloReply
             {
                 Message = "Hello " + request.Name
-            };
+            });
         }
     }
 }
