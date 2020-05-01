d5654234a0be352b72a7326af84266de1cdfa635
Stephen Toub <stoub@microsoft.com>
2020-04-30 21:25:24 -0400

    Tweak MakeRawGrpcCall to reduce noise
    I made some changes locally to reduce noise while looking at the actual underlying SocketsHttpHandler implementation.  Figured I'd push 'em up.

--- GrpcSampleClient/Program.cs
+++ GrpcSampleClient/Program.cs
@@ -5,13 +5,12 @@ using GrpcSample;
 using Microsoft.IO;
 using System;
 using System.Buffers.Binary;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
-using System.Net.Http.Headers;
+using System.Runtime;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -22,6 +21,8 @@ namespace GrpcSampleClient
         private static readonly RecyclableMemoryStreamManager StreamPool = new RecyclableMemoryStreamManager();
         private static readonly Dictionary<int, Greeter.GreeterClient> GrpcClientCache = new Dictionary<int, Greeter.GreeterClient>();
         private static readonly Dictionary<int, HttpClient> HttpClientCache = new Dictionary<int, HttpClient>();
+        private static readonly Dictionary<int, HttpMessageInvoker> HttpMessageInvokerCache = new Dictionary<int, HttpMessageInvoker>();
+        private static Uri RawGrpcUri= new Uri($"http://localhost:5001/greet.Greeter/SayHello");
         private static bool ClientPerThread;
 
         static async Task Main(string[] args)
@@ -34,7 +35,7 @@ namespace GrpcSampleClient
             long lastElapse = 0;
             Exception lastError = null;
 
-            _ = Task.Run(async () =>
+            new Thread(() =>
             {
                 long pastRequests = 0;
                 while (true)
@@ -47,9 +48,10 @@ namespace GrpcSampleClient
                         Console.WriteLine(ex.ToString());
                     }
                     pastRequests = successCounter;
-                    await Task.Delay(1000);
+                    Thread.Sleep(1000);
                 }
-            });
+            })
+            { IsBackground = true }.Start();
 
             Func<int, Task> request;
             string clientType;
@@ -65,17 +67,17 @@ namespace GrpcSampleClient
             }
             else if (args[0] == "r")
             {
-                request = (i) => MakeRawGrpcCall(new HelloRequest() { Name = "foo" }, GetHttpClient(i, 5001));
-                clientType = "Raw HttpClient";
+                request = (i) => MakeRawGrpcCall(new HelloRequest() { Name = "foo" }, GetHttpMessageInvoker(i));
+                clientType = "Raw HttpMessageInvoker";
             }
             else if (args[0] == "h2")
             {
-                request = (i) => MakeHttpCall(new HelloRequest() { Name = "foo" }, GetHttpClient(i, 5001), new Version(2, 0));
+                request = (i) => MakeHttpCall(new HelloRequest() { Name = "foo" }, GetHttpClient(i, 5001), HttpVersion.Version20);
                 clientType = "HttpClient+HTTP/2";
             }
             else if (args[0] == "h1")
             {
-                request = (i) => MakeHttpCall(new HelloRequest() { Name = "foo" }, GetHttpClient(i, 5000), new Version(1, 1));
+                request = (i) => MakeHttpCall(new HelloRequest() { Name = "foo" }, GetHttpClient(i, 5000), HttpVersion.Version11);
                 clientType = "HttpClient+HTTP/1.1";
             }
             else
@@ -86,6 +88,8 @@ namespace GrpcSampleClient
 
             var parallelism = int.Parse(args[1]);
             Console.WriteLine("Parallelism: " + parallelism);
+            Console.WriteLine($"{nameof(GCSettings.IsServerGC)}: {GCSettings.IsServerGC}");
+
             await Task.WhenAll(Enumerable.Range(0, parallelism).Select(async i =>
             {
                 Stopwatch s = Stopwatch.StartNew();
@@ -154,10 +158,26 @@ namespace GrpcSampleClient
             return client;
         }
 
+        private static HttpMessageInvoker GetHttpMessageInvoker(int i)
+        {
+            if (!ClientPerThread)
+            {
+                i = 0;
+            }
+            if (!HttpMessageInvokerCache.TryGetValue(i, out var client))
+            {
+                AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+                client = new HttpMessageInvoker(new SocketsHttpHandler { AllowAutoRedirect = false, UseProxy = false });
+                HttpMessageInvokerCache.Add(i, client);
+            }
+
+            return client;
+        }
+
         private static Greeter.GreeterClient GetGrpcNetClient(string host, int port)
         {
             AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
-            var httpClient = new HttpClient();
+            var httpClient = new HttpClient() { Timeout = Timeout.InfiniteTimeSpan };
             var baseUri = new UriBuilder
             {
                 Scheme = Uri.UriSchemeHttp,
@@ -194,34 +214,33 @@ namespace GrpcSampleClient
                 Content = new StreamContent(memStream),
                 Version = httpVersion
             };
-            httpRequest.Content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+            httpRequest.Content.Headers.TryAddWithoutValidation("Content-Type", "application/octet-stream");
             using var httpResponse = await client.SendAsync(httpRequest);
             var responseStream = await httpResponse.Content.ReadAsStreamAsync();
             return HelloReply.Parser.ParseDelimitedFrom(responseStream);
         }
 
-        private static async Task<HelloReply> MakeRawGrpcCall(HelloRequest request, HttpClient client)
+        private static async Task<HelloReply> MakeRawGrpcCall(HelloRequest request, HttpMessageInvoker client)
         {
             var messageSize = request.CalculateSize();
-            var messageBytes = new byte[messageSize];
-            request.WriteTo(new CodedOutputStream(messageBytes));
-
             var data = new byte[messageSize + 5];
+            request.WriteTo(new CodedOutputStream(data));
+
+            Array.Copy(data, 0, data, 5, messageSize);
             data[0] = 0;
             BinaryPrimitives.WriteUInt32BigEndian(data.AsSpan(1, 4), (uint)messageSize);
-            messageBytes.CopyTo(data.AsSpan(5));
 
-            using var httpRequest = new HttpRequestMessage(HttpMethod.Post, "/greet.Greeter/SayHello");
-            httpRequest.Version = new Version(2, 0);
-            httpRequest.Content = new StreamContent(new MemoryStream(data));
-            httpRequest.Content.Headers.ContentType = new MediaTypeHeaderValue("application/grpc");
-            httpRequest.Headers.TE.Add(new TransferCodingWithQualityHeaderValue("trailers"));
+            using var httpRequest = new HttpRequestMessage(HttpMethod.Post, RawGrpcUri);
+            httpRequest.Version = HttpVersion.Version20;
+            httpRequest.Content = new ByteArrayContent(data);
+            httpRequest.Content.Headers.TryAddWithoutValidation("Content-Type", "application/grpc");
+            httpRequest.Headers.TryAddWithoutValidation("TE", "trailers");
 
-            var response = await client.SendAsync(httpRequest, HttpCompletionOption.ResponseHeadersRead);
+            using var response = await client.SendAsync(httpRequest, default);
             response.EnsureSuccessStatusCode();
 
             data = await response.Content.ReadAsByteArrayAsync();
-            var responseMessage = HelloReply.Parser.ParseFrom(data.AsSpan(5, data.Length - 5).ToArray());
+            var responseMessage = HelloReply.Parser.ParseFrom(data.AsSpan(5).ToArray());
 
             var grpcStatus = response.TrailingHeaders.GetValues("grpc-status").SingleOrDefault();
             if (grpcStatus != "0")
